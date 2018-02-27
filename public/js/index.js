0d9083166844042e1284ea7d0074b973ec00b342
System Administrator <root@Nikolas-MacBook-Pro.local>
2018-02-27 13:42:56 +0100

    Add geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,18 @@ socket.on('newMessage', function(message) {
     $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+    console.log('newMessage', message);
+    var li = $('<li></li>');
+    var a = $('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    
+    $('#messages').append(li);
+});
+
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
 
@@ -25,5 +37,21 @@ $('#message-form').on('submit', function(e) {
         text: jQuery('[name=message]').val()
     }, function() {
 
+    });
+});
+
+var locationBtn = $('#send-location');
+locationBtn.on('click', function() {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not supported!');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        alert('Unable to fetch location!');
     });
 });
